f2fa296c3d54d778f21ef07db0a35e52c58f0680
doctor-adayum <ajacob7111997@gmail.com>
2017-05-29 00:06:49 +1000

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav({closeOnClick: true, menuWidth: 250});
+    $('.parallax').parallax();
+    $('.modal-trigger').leanModal();
+    $('.scrollspy').scrollSpy({scrollOffset: 56});
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+Contact GitHub API Training Shop Blog About
