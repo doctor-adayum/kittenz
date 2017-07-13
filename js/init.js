b5a5e921c3424af9b7ea106869aa46e5b8a3cd24
doctor-adayum <ajacob7111997@gmail.com>
2017-07-13 23:42:40 +1000

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,16 @@
+(function($){
+  $(function(){
+
+    $('.tap-target').tapTarget('open');
+    $(".button-collapse").sideNav({
+      menuWidth: 240, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+    });
+    $('.parallax').parallax();
+
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
